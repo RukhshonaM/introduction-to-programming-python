381890f71883916b75b5ec4fb55e960ba61a6183
Rukhshona <rukhshona_mirzarahmatova@student.itpu.uz>
2024-10-18 12:16:07 +0300

    Merge Rukhshona_Mir._2group into main and resolve conflicts

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 nick = input('r: ')
 print('hi, {}' .format(nick))
 "I'm learning Git merge conflicts" 
+print("I'm learning Git merge conflicts")
+first_name = input('Rukhshona: ')
+last_name = input('Mir.: ')
+nick = input('r: ')
+print('Hi, {} {} a.k.a. {}!'.format(first_name, last_name, nick))
